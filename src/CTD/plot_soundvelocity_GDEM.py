26929723c3695d4e816862b9bcc96b39330e7065
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-05-08 16:35:42 -0400

    updating code

--- src/CTD/plot_soundvelocity_GDEM.py
+++ src/CTD/plot_soundvelocity_GDEM.py
@@ -5,12 +5,16 @@ import matplotlib.pyplot as plt
 import pyproj
 
 # charger le fichier .mat
-data = scipy.io.loadmat('GDEM/Jun_GDEMV.mat')
-map = scipy.io.loadmat('GDEM/geomap_GDEMV.mat')
-print(data)
+data = scipy.io.loadmat('../../data/GDEM/Jun_GDEMV.mat')
+map = scipy.io.loadmat('../../data/GDEM/geomap_GDEMV.mat')
+print(map)
 
-salinity=data['salinity']*0.001+15
+salinity=data['salinity']
 temp=data['water_temp']*0.001+15
+
+salinity[salinity==-32000]=np.nan
+salinity=salinity*0.001+15
+print(salinity)
 
 print(salinity-temp)
 print(temp)
@@ -26,15 +30,17 @@ print(depth.shape)
 
 # cast2
 lat_cast2 = 31 + (26.95/60)
+print(lat_cast2)
 lon_cast2 = - 68 + (41.58/60)
-lat_deg, lon_deg = pyproj.transform(wgs84, epsg3035, lon_cast2, lat_cast2)
+print(lon_cast2)
 
-lat_idx = np.abs(lat-lat_cast2).argmin()
-lon_idx = np.abs(lon-lon_cast2).argmin()
+wgs84 = pyproj.CRS('EPSG:4326')
+latlong = pyproj.CRS('EPSG:4269')
+lat_deg, lon_deg = pyproj.transform(wgs84, latlong, lon_cast2, lat_cast2)
 
-print(lat_idx)
-print(lon_idx)
-print(lon+lon_cast2)
+lat_idx = np.abs(lat-lat_deg).argmin()
+lon_idx = np.abs(lon-lon_deg).argmin()
+
 
 print(lat_idx)
 print(lon_idx)
@@ -45,5 +51,5 @@ temp_cast2 = temp[:, lat_idx, lon_idx]
 salinity_cast2 = salinity[:, lat_idx, lon_idx]
 
 
-plt.plot(salinity_cast2,-depth)
+plt.plot(temp_cast2,depth)
 plt.show()
