c1fd7dcaaa9d41f477b87c2a6a7409e7eddf453a
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-06-21 18:38:39 -0400

    Update

--- src/ray_tracing/ray_dist.py
+++ src/ray_tracing/ray_dist.py
@@ -1,7 +1,51 @@
+# import numpy as np
+# import scipy.io as sio
+# import math
+# from analytic_sol import *
+# from scipy.optimize import minimize
+# from tqdm import tqdm
+#
+# def dist(xyzS, xyzR):
+#     '''
+#     Inputs :
+#         xyzS : position of the source
+#         xyzR : position of the receiver
+#     Outputs :
+#         dist : distance between source and receiver
+#         time : travel time
+#     '''
+#     xs, ys, zs = xyzS[0], xyzS[1], xyzS[2]
+#     xr, yr, zr = xyzR[0], xyzR[1], xyzR[2]
+#     alpha = np.arange(9.95, 10.00, 0.001)
+#     progress_bar = tqdm(total=len(alpha), desc="Progress", unit="alpha")
+#     for i in range(len(alpha)):
+#         x, z = rayPathLSODA(alpha[i], zs, xs, tMax=40, dt=0.01)
+#         t = np.arange(0,400,0.01)
+#         errors = np.sqrt((xr - x) ** 2 + (zr - z) ** 2)
+#         id_min = np.nanargmin(errors)
+#         err_min = np.nanmin(errors)
+#         if err_min < 2 :
+#             dist = np.sqrt((xr-x[id_min])**2 + (zr - z[id_min])**2)
+#             plt.scatter(xs, zs, color='green', label='Source')
+#             plt.scatter(xr, zr, color='red', label='Receiver')
+#             plt.plot(x, z, label='Alpha = {}°'.format(alpha[i]))
+#             plt.gca().invert_yaxis()
+#             plt.title("Ray Path in Linear Wavespeed profile")
+#             plt.xlabel("Horizontal Distance (km)")
+#             plt.ylabel("Depth (km)")
+#             plt.legend()
+#             plt.show()
+#             return (print('xcalc={} and zcalc={} at t={} for alpha={} and dist ={}'.format(x[id_min], z[id_min], t[id_min], alpha[i], dist)))
+#         progress_bar.update(1)
+#     return (print('No ray reaches the receiver, change angle or tolerance threshold'))
+#
+# if __name__ == '__main__':
+#     dist([0, 0, 0], [22761, 3000, 5025])
+
 import numpy as np
 import scipy.io as sio
 import math
-from analytic_sol import *
+from numeric_sol import *
 from scipy.optimize import minimize
 from tqdm import tqdm
 
@@ -18,14 +62,23 @@ def dist(xyzS, xyzR):
     xr, yr, zr = xyzR[0], xyzR[1], xyzR[2]
     alpha = np.arange(9.95, 10.00, 0.001)
     progress_bar = tqdm(total=len(alpha), desc="Progress", unit="alpha")
+
+    dist_min = float('inf')
+    x_min, z_min = None, None
+
     for i in range(len(alpha)):
         x, z = rayPathLSODA(alpha[i], zs, xs, tMax=40, dt=0.01)
-        t = np.arange(0,400,0.01)
+        t = np.arange(0, 400, 0.01)
         errors = np.sqrt((xr - x) ** 2 + (zr - z) ** 2)
         id_min = np.nanargmin(errors)
         err_min = np.nanmin(errors)
-        if err_min < 2 :
-            dist = np.sqrt((xr-x[id_min])**2 + (zr - z[id_min])**2)
+
+        if err_min < 2:
+            dist = np.sqrt((xr - x[id_min]) ** 2 + (zr - z[id_min]) ** 2)
+            if dist < dist_min:
+                dist_min = dist
+                x_min, z_min = x[id_min], z[id_min]
+
             plt.scatter(xs, zs, color='green', label='Source')
             plt.scatter(xr, zr, color='red', label='Receiver')
             plt.plot(x, z, label='Alpha = {}°'.format(alpha[i]))
@@ -35,9 +88,13 @@ def dist(xyzS, xyzR):
             plt.ylabel("Depth (km)")
             plt.legend()
             plt.show()
-            return (print('xcalc={} and zcalc={} at t={} for alpha={} and dist ={}'.format(x[id_min], z[id_min], t[id_min], alpha[i], dist)))
+
         progress_bar.update(1)
-    return (print('No ray reaches the receiver, change angle or tolerance threshold'))
+
+    if x_min is not None and z_min is not None:
+        print('xcalc={} and zcalc={} for alpha={} and dist ={}'.format(x_min, z_min, alpha[i], dist_min))
+    else:
+        print('No ray reaches the receiver, change angle or tolerance threshold')
 
 if __name__ == '__main__':
     dist([0, 0, 0], [22761, 3000, 5025])
