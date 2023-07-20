297a19796e8b28cd759b70c7298a3e53e43b8bb2
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-07-20 15:27:03 -0400

    Ray tracing with analytic solution

--- src/ray_tracing/analytic_sol.py
+++ src/ray_tracing/analytic_sol.py
@@ -1,36 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
+from scipy.interpolate import interp1d
+from numba import jit
+from scipy.interpolate import CubicSpline
 
-def ray(x0, cz, depth, theta0):
-    z=np.linspace(0,5200,1000)
-    f=scipy.interpolate.interp1d(depth,cz,'cubic')
-    sv=f(z)
-    depth=z
-    cz=sv
+sv=np.loadtxt('../../data/sv_GDEM.txt')
+depth=np.loadtxt('../../data/depth_GDEM.txt')
+
+np.seterr(invalid='ignore')
+
+def ray_analytic(cz, depth, theta0):
+    '''
+    Inputs:
+        cz: Sound speed profile
+        depth: Depth values
+        theta0: Incident angle in degrees
+    Outputs:
+        x: x-coordinates of the ray path
+        z: Depth values
+        t: Travel time along the ray path
+    '''
+    # Generate depth values
+    z = np.linspace(0, np.max(depth) + 25.0, int(1e6))
+
+    # Interpolate sound speed profile
+    f = interp1d(depth, cz, 'linear', bounds_error=False, fill_value='extrapolate')
+    cz = f(z)
+    depth = z
+
+    # Calculate the gradient of the sound speed profile
     gradc = np.diff(cz) / np.diff(depth)
     gradc = np.insert(gradc, 0, gradc[0])
-    v = [cz[0]]
-    xk, tk = x0, depth[0], 0
-    x, z, t = [xk], [zk], [tk]
-    theta0=(90-theta0)*np.pi/180
-    p = np.sin(theta0) / v[0]
-    for k in range(1,len(cz)):
-        v.append(v[k-1] + gradc[k] * (depth[k]-depth[k-1]))
-        xk += 1 / (p * gradc[k]) * (np.sqrt(1 - p**2 * v[k-1]**2) - np.sqrt(1 - p**2 * v[k]**2))
-        x.append(xk)
-        tk+=1 / gradc[k] * np.log((v[k] / v[k-1]) * ((1 + np.sqrt(1 - p**2 * v[k-1]**2)) / (1 + np.sqrt(1 - p**2 * v[k]**2))))
-        t.append(tk)
-    plt.plot(x,depth,label='{}°'.format(theta0*180/np.pi))
-    plt.title("Ray Path in Linear Wavespeed profile")
-    plt.xlabel("Horizontal Distance (km)")
-    plt.ylabel("Depth (km)")
-    plt.legend()
-    plt.gca().invert_yaxis()
-    plt.show()
-    return x, t
+
+    # Convert incident angle to radians
+    theta0 = (90 - theta0) * np.pi / 180
+    # Calculate the ray parameter
+    p = np.sin(theta0) / cz[0]
+
+    # Calculate the horizontal positions along the ray path
+    xk = np.cumsum(1 / (p * gradc[:-1]) * (np.sqrt(1 - p**2 * cz[:-1]**2) - np.sqrt(1 - p**2 * cz[1:]**2)))
+    # Calculate the travel times along the ray path
+    tk = np.cumsum(1 / gradc[:-1] * np.log((cz[1:] / cz[:-1]) * ((1 + np.sqrt(1 - p**2 * cz[:-1]**2)) / (1 + np.sqrt(1 - p**2 * cz[1:]**2)))))
+
+    # Combine the source position with the initial values
+    x = np.concatenate(([0], xk))
+    t = np.concatenate(([0], tk))
+
+    # Return the x-coordinates, depth values, and travel times
+    return x, z, t
+
 
 if __name__=='__main__':
     sv=np.loadtxt('../../data/sv_GDEM.txt')
-    depth=-np.loadtxt('../../data/depth_GDEM.txt')
-    x,z,t=ray(0,sv,depth,10)
+    depth=np.loadtxt('../../data/depth_GDEM.txt')
+    x, z, t=ray_analytic(sv,depth,10)
