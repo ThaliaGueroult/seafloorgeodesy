8e0b203baa5e66fcc3e2c595772f19a768286719
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-07-24 15:13:06 -0400

    Compute elevation angle and vertical distance from a source and a receiver

--- src/esv/xyz2dec.py
+++ src/esv/xyz2dec.py
@@ -3,7 +3,19 @@ import matplotlib.pyplot as plt
 import scipy
 from geodesy_toolbox import *
 
+# This script calculates the intermediate point and vertical distance
+# between a source (xyzS) and a receiver (xyzR) using geodetic coordinates.
+
 def calculate_intermediate_point(xyzS, xyzR):
+    '''
+    Inputs:
+    xyzS - Source coordinates [longitude, latitude, altitude]
+    xyzR - Receiver coordinates [longitude, latitude, altitude]
+
+    Outputs:
+    xyzI - Intermediate point coordinates [longitude, latitude, altitude]
+    '''
+    # Convert source and receiver coordinates from geodetic to ECEF
     xs, ys, zs = geod2ecef(xyzS[0], xyzS[1], xyzS[2])
     xr, yr, zr = geod2ecef(xyzR[0], xyzR[1], xyzR[2])
 
@@ -20,19 +32,29 @@ def calculate_intermediate_point(xyzS, xyzR):
     return xyzI
 
 def xyz2dec(xyzS, xyzR):
+    '''
+    Inputs:
+    xyzS - Source coordinates [longitude, latitude, altitude]
+    xyzR - Receiver coordinates [longitude, latitude, altitude]
+
+    Outputs:
+    dz - Vertical distance between source and receiver
+    beta - Elevation angle from source to receiver
+    '''
+    # Calculate the intermediate point coordinates
     xyzI = calculate_intermediate_point(xyzS, xyzR)
     xs, ys, zs = xyzS[0], xyzS[1], xyzS[2]
     xi, yi, zi = xyzI[0], xyzI[1], xyzI[2]
 
-    # Calcul de la distance verticale
+    # Calculate the vertical distance
     dz = zi - zs
 
-    # Calcul de la distance horizontale
+    # Calculate the horizontal distance
     dx = xi - xs
     dy = yi - ys
     distance_horizontale = np.sqrt(dx**2 + dy**2)
 
-    # Calcul de l'angle d'élévation
+    # Calculate the elevation angle
     beta = math.degrees(math.atan2(dz, distance_horizontale))
 
     return dz, beta
