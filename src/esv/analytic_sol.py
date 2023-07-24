9be5ccbeb6692df0c59c36b9d7d193fe7cc08238
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-07-24 15:18:44 -0400

    Analytic Solution of Eikonal equation in Sea Water with elevation angle and vertical distance as input

--- src/esv/analytic_sol.py
+++ src/esv/analytic_sol.py
@@ -1,38 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
+import scipy.io as sio
 from scipy.interpolate import interp1d
 from numba import jit
 from scipy.interpolate import CubicSpline
 
-sv=np.loadtxt('../../data/sv_GDEM.txt')
-depth=np.loadtxt('../../data/depth_GDEM.txt')
+cz = np.loadtxt('../../data/cz_interp5250.txt')
+depth = np.loadtxt('../../data/depth_interp5250.txt')
+gradc = np.loadtxt('../../data/gradc_interp5250.txt')
 
-np.seterr(invalid='ignore')
+# sv=np.loadtxt('../../data/sv_GDEM.txt')
+# depthg=np.loadtxt('../../data/depth_GDEM.txt')
+#
+# def preprocess_data(cz, depth):
+#     z = np.linspace(0, np.max(depth) + 50, int(1e6)+4784)
+#     f = interp1d(depth, cz, 'linear', bounds_error=False, fill_value='extrapolate')
+#     cz = f(z)
+#     depth = z
+#     gradc = np.diff(cz) / np.diff(depth)
+#     gradc = np.insert(gradc, 0, gradc[0])
+#     return cz, depth, gradc
+#
+# cz2,depth2,gradc2 = preprocess_data(sv,depthg)
+# np.savetxt('cz_interp5250.txt',cz2)
+# np.savetxt('depth_interp5250.txt',depth2)
+# np.savetxt('gradc_interp5250.txt',gradc2)
 
-def ray_analytic(cz, depth, theta0):
-    '''
-    Inputs:
-        cz: Sound speed profile
-        depth: Depth values
-        theta0: Incident angle in degrees
-    Outputs:
-        x: x-coordinates of the ray path
-        z: Depth values
-        t: Travel time along the ray path
-    '''
-    # Generate depth values
-    z = np.linspace(0, np.max(depth) + 25.0, int(1e6))
-
-    # Interpolate sound speed profile
-    f = interp1d(depth, cz, 'linear', bounds_error=False, fill_value='extrapolate')
-    cz = f(z)
-    depth = z
-
-    # Calculate the gradient of the sound speed profile
-    gradc = np.diff(cz) / np.diff(depth)
-    gradc = np.insert(gradc, 0, gradc[0])
-
+def ray_analytic(cz, depth, gradc, theta0):
     # Convert incident angle to radians
     theta0 = (90 - theta0) * np.pi / 180
     # Calculate the ray parameter
@@ -48,10 +42,51 @@ def ray_analytic(cz, depth, theta0):
     t = np.concatenate(([0], tk))
 
     # Return the x-coordinates, depth values, and travel times
-    return x, z, t
+    return x, depth, t
 
 
 if __name__=='__main__':
-    sv=np.loadtxt('../../data/sv_GDEM.txt')
-    depth=np.loadtxt('../../data/depth_GDEM.txt')
-    x, z, t=ray_analytic(sv,depth,10)
+    cz = np.loadtxt('../../data/cz_interp.txt')
+    depth = np.loadtxt('../../data/depth_interp.txt')
+    gradc = np.loadtxt('../../data/gradc_interp.txt')
+
+    # sv=np.loadtxt('../../data/sv_GDEM.txt')
+    # depthg=np.loadtxt('../../data/depth_GDEM.txt')
+    #
+    # plt.scatter(gradc[-10000:-9500], depth[-10000:-9500] , label = "Avec interp")
+    # # plt.scatter(sv,depthg, label = "Sans Interp")
+    # plt.xlabel('Vitesse du son en m/s')
+    # plt.ylabel('Profondeur en m')
+    # plt.gca().invert_yaxis()
+    # plt.legend()
+    # plt.show()
+
+    # data = sio.loadmat('data_5225_step_angle_2.mat')
+    # esv = data['c_esv']
+    # angle = data['list_beta']
+    # plt.scatter(angle,esv, label='5225 m')
+    # data = sio.loadmat('data_5227_step_angle_2.mat')
+    # esv = data['c_esv']
+    # angle = data['list_beta']
+    # plt.scatter(angle,esv, label='5227 m')
+    # data = sio.loadmat('data_5223_step_angle_2.mat')
+    # esv = data['c_esv']
+    # angle = data['list_beta']
+    # plt.scatter(angle,esv, label='5223 m')
+    # data = sio.loadmat('data_5220_step_angle_2.mat')
+    # esv = data['c_esv']
+    # angle = data['list_beta']
+    # plt.scatter(angle,esv, label='5220 m')
+    # data = sio.loadmat('data_5200_step_angle_2.mat')
+    # esv = data['c_esv']
+    # angle = data['list_beta']
+    # plt.scatter(angle,esv, label='5200 m')
+    # plt.legend()
+    # plt.xlabel("Angle d'élévation en degrés")
+    # plt.ylabel("Vitesse équivalente en m/s")
+    #
+    # plt.show()
+
+    # x, z, t=ray_analytic(cz,depth,gradc,45)
+    # plt.plot(x,z)
+    # plt.show()
