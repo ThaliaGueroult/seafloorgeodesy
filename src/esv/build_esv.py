27e7d83512fade9538c25c86bca9ffec503bfb3d
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-08-07 15:58:48 -0400

    Construct an ESV table

--- src/esv/build_esv.py
+++ src/esv/build_esv.py
@@ -3,6 +3,7 @@ import scipy.io as sio
 from scipy.io import savemat
 import math
 from scipy.optimize import minimize
+from scipy.interpolate import interp1d
 from tqdm import tqdm
 from analytic_sol import *
 import multiprocessing as mp
@@ -76,106 +77,6 @@ def dist_analytic(dz, beta):
 
     return c_esv
 
-
-# def dist_analytic(dz, beta):
-#     '''
-#     Inputs :
-#         dz : vertical distance between source and receiver
-#         beta : elevation angle between source and receiver
-#     Outputs :
-#         time : travel time
-#         c_esv : effective sound velocity between source and receiver
-#     '''
-#
-#     # Compute horizontal distance between source and receiver
-#     beta = beta*np.pi/180
-#     dh = dz / np.tan(beta)
-#     xr, zr = dh, dz
-#
-#     # Set initial parameters
-#     alpha_range = np.linspace(0, 90, 100)
-#     alpha_step = (max(alpha_range) - min(alpha_range)) / len(alpha_range)
-#     tolerance = 0.003
-#
-#     # Initialize variables for tracking minimum distance and associated parameters
-#     dist_min = np.inf
-#     x_min, z_min = None, None
-#     t_receiver = None
-#     best_alpha = None
-#     x_fin = np.empty(0)
-#     z_fin = np.empty(0)
-#
-#     # Set flags for plotting and early termination
-#     plot_results = False
-#     early_termination = False
-#
-#     while alpha_step > 10e-10:
-#         alpha = np.array(alpha_range)
-#         # progress_bar = tqdm(total=len(alpha), desc="Progress", unit="alpha")
-#
-#         errors_all = np.empty((len(alpha),))
-#         x_all = np.empty((len(alpha),))
-#         z_all = np.empty((len(alpha),))
-#         t_all = np.empty((len(alpha),))
-#
-#         for i, angle in enumerate(alpha):
-#             # Calculate ray path for the given angle
-#             x, z, t = ray_analytic(cz, depth, gradc, angle)
-#
-#             # Calculate errors and find minimum error index
-#             errors = np.sqrt((xr - x) ** 2 + (zr - z) ** 2)
-#             errors_all[i] = np.nanmin(errors)
-#             id_min = np.nanargmin(errors)
-#
-#             # Store corresponding coordinates and time for minimum error
-#             x_all[i] = x[id_min]
-#             z_all[i] = z[id_min]
-#             t_all[i] = t[id_min]
-#
-#             # Calculate distance and update minimum distance if necessary
-#             dist = np.sqrt((xr - x_all[i]) ** 2 + (zr - z_all[i]) ** 2)
-#             if dist < dist_min:
-#                 dist_min = dist
-#                 x_min, z_min = x_all[i], z_all[i]
-#                 best_alpha = angle
-#                 t_receiver = t_all[i]
-#
-#             # progress_bar.update(1)
-#
-#             # Check if tolerance condition is met for early termination
-#             if errors_all[i] < tolerance and x_min is not None and z_min is not None:
-#                 x_fin, z_fin, t_fin = x, z, t
-#                 early_termination = True
-#                 break
-#
-#         # print(dist_min)
-#         # progress_bar.close()
-#
-#         if early_termination :
-#             if beta == 0 :
-#                 d_sr = dz
-#                 c_esv = dz / t_receiver
-#                 print('xcalc={} and zcalc={} for alpha={}, t_receiver = {} s and dist ={} with c_esv = {}m/s'.format(x_min, z_min, best_alpha, t_receiver, dist_min, c_esv))
-#                 return c_esv
-#
-#             else :
-#                 d_sr = dz / np.sin(beta)
-#                 c_esv = d_sr / t_receiver
-#                 # print('xcalc={} and zcalc={} for alpha={}, t_receiver = {} s and dist ={} with c_esv = {}m/s'.format(x_min, z_min, best_alpha, t_receiver, dist_min, c_esv))
-#                 print(c_esv)
-#                 return c_esv
-#
-#         # Update previous minimum distance and adjust alpha range for the next iteration
-#         alpha_range = np.linspace(best_alpha - alpha_step, best_alpha + alpha_step, 100)
-#         alpha_step /= 2
-#
-#     # If no ray reaches the receiver, print a message
-#     if x_min is None or z_min is None:
-#         # return('No ray reaches the receiver, change angle or tolerance threshold, dist = {}'.format(dist_min))
-#         return(np.nan)
-
-
-
 def process_beta(beta, dz):
     return dist_analytic(dz, beta)
 
@@ -199,7 +100,7 @@ def table_angle(dz, step):
 def plot_esv():
 
     # Numéros de fichiers de 5226 à 5249
-    file_numbers = range(5201, 5227)
+    file_numbers = range(5182, 5197)
 
     # Initialisation des listes pour les données des fichiers
     all_angles = []
@@ -236,29 +137,77 @@ def plot_esv():
     plt.ylabel('ESV')
     plt.show()
 
-if __name__ == '__main__':
-    # d = 5241
-    # for k in range(10):
-    #     d+=1
-    #     table_angle(d,2)
-    plot_esv()
+def plot_and_save_esv():
+    # Numéros de fichiers de 5226 à 5249
+    file_numbers = range(5182, 5246)
 
-    # data = sio.loadmat('esv_table_without_tol/data_5201_step_angle_2.mat')
-    # esv = data['c_esv']
-    # angle = data['list_beta']
-    # plt.scatter(angle,esv, label = '5201 m')
-    # plt.legend()
-    # plt.title('Effective Sound Velocity')
-    # data = sio.loadmat('esv_table_without_tol/data_5202_step_angle_2.mat')
-    # print(data)
-    # esv = data['c_esv']
-    # angle = data['list_beta']
-    # plt.scatter(angle,esv, label = '5202 m')
-    # plt.ticklabel_format(axis='y', style='plain')
-    # data = sio.loadmat('esv_table_without_tol/data_5203_step_angle_2.mat')
-    # print(data)
-    # esv = data['c_esv']
-    # angle = data['list_beta']
-    # plt.scatter(angle,esv, label = '5203 m')
-    # plt.ticklabel_format(axis='y', style='plain')
-    # plt.show()
+    # Liste de labels et de couleurs
+    labels = []
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(file_numbers)))
+
+
+    for i, file_number in enumerate (file_numbers):
+        # Nom du fichier
+        file_name = f"esv_table_without_tol/data_{file_number}_step_angle_2.mat"
+
+        # Ajouter le label et la couleur du fichier
+        labels.append(f"File {file_number}")
+
+        # Charger les données depuis le fichier
+        data = sio.loadmat(file_name)
+        esv = data['c_esv']
+        angle = data['list_beta']
+
+        # Interpolation de 45 à 90 degrés avec un step de 0.01 degrés
+        new_angles = np.arange(45, 90, 0.01)
+        f = interp1d(angle.flatten(), esv.flatten(), kind='cubic',bounds_error=False, fill_value='extrapolate')
+        new_esv = f(new_angles)
+
+        # Enregistrer les données interpolées dans un fichier avec le nom reflétant le step de 0.01
+        new_file_name = f"esv_table_without_tol/data_{file_number}_step_angle_0_01.csv"
+        np.savetxt(new_file_name, np.column_stack((new_angles, new_esv)), delimiter=',')
+
+        plt.plot(new_angles, new_esv, label=labels[i], color=colors[i])
+
+    plt.xlabel('Angle (degrees)')
+    plt.ylabel('ESV')
+    plt.title('Interpolated ESV data')
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0., ncol = 2)
+    plt.show()
+
+
+def compare_interpolations(file_number=5225):
+    # Charger les données depuis le fichier
+    file_name = f"esv_table_without_tol/data_{file_number}_step_angle_2.mat"
+    data = sio.loadmat(file_name)
+    esv = data['c_esv']
+    angle = data['list_beta']
+
+    # Interpolation de 45 à 90 degrés avec un step de 0.01 degrés
+    new_angles = np.arange(45, 90, 0.01)
+
+    # Liste des méthodes d'interpolation possibles
+    # interpolation_methods = ['linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic']
+    interpolation_methods = ['linear', 'nearest', 'quadratic', 'cubic']
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(interpolation_methods)))
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(angle, esv, label='Original Data', color='red', marker='x')
+
+    for method, color in zip(interpolation_methods, colors):
+        f = interp1d(angle.flatten(), esv.flatten(), kind=method, bounds_error=False, fill_value='extrapolate')
+        new_esv = f(new_angles)
+        plt.plot(new_angles, new_esv, label=f'{method.capitalize()} Interpolation', color=color)
+
+    plt.xlabel('Angle (degrees)')
+    plt.ylabel('ESV')
+    plt.title(f'Interpolation Comparison for File {file_number}')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    table_angle(5181,2)
+
+    compare_interpolations(file_number=5225)
