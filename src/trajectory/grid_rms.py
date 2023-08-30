43319fbc508e45131432300fbdad16c319a2e167
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-08-30 14:16:28 -0400

    RMS computation from a grid of receiver

--- src/trajectory/grid_rms.py
+++ src/trajectory/grid_rms.py
@@ -5,6 +5,7 @@ import scipy.io as sio
 from pymap3d import geodetic2ecef, ecef2geodetic
 import os
 from multiprocessing import Pool
+import time
 
 # Load the Earth-to-Space Volume (ESV) matrix from a .mat file
 # Path to the folder containing the .mat file
@@ -20,8 +21,8 @@ angle_array = data['angle'].flatten()
 esv_matrix = data['matrice']
 
 
-def create_grid_around(coord, extension=20.0):
-    step = 10.0 # 1 cm
+def create_grid_around(coord, extension=5.0):
+    step = 1.0 # 1 mm
     range_vals = np.arange(-extension, extension + step, step)
     print(range_vals)
     # Créez une grille 3D
@@ -37,61 +38,31 @@ def create_grid_around(coord, extension=20.0):
 
     return grid_points
 
-
-# Function to calculate intermediate point between Source (S) and Receiver (R)
-def calculate_intermediate_point(xyzS, xyzR):
-    """
-    This function calculates the intermediate point coordinates between the source and receiver.
-    Input:
-        xyzS: [lat, lon, alt] of Source
-        xyzR: [lat, lon, alt] of Receiver
-    Output:
-        Intermediate point coordinates
-    """
-    # Convert lat, lon, alt to Earth-Centered Earth-Fixed (ECEF) coordinates
-    xs, ys, zs = geodetic2ecef(xyzS[0], xyzS[1], xyzS[2])
-    xr, yr, zr = geodetic2ecef(xyzR[0], xyzR[1], xyzR[2])
-
-    # Calculate the distance between the source and receiver
-    d = np.sqrt((xr - xs) ** 2 + (yr - ys) ** 2)
-
-    # Calculate the coordinates of the intermediate point I
-    xi = xs + d
-    yi = ys
-    zi = zr
-
-    xyzI = ecef2geodetic(xi, yi, zr)
-
-    return xyzI
-
 # Function to convert source and receiver coordinates to declination angle and elevation
 def xyz2dec(xyzS, xyzR):
     '''
     Inputs:
-    xyzS - Source coordinates [longitude, latitude, altitude]
-    xyzR - Receiver coordinates [longitude, latitude, altitude]
+        xyzS - Source coordinates [longitude, latitude, altitude]
+        xyzR - Receiver coordinates [longitude, latitude, altitude]
 
     Outputs:
-    dz - Vertical distance between source and receiver
-    beta - Elevation angle from source to receiver
+        dz - Vertical distance between source and receiver (m)
+        beta - Elevation angle from source to receiver (°)
     '''
-    elevS=xyzS[2]
-    elevR=xyzR[2]
     # Calculate the intermediate point coordinates
-    xyzI = calculate_intermediate_point(xyzS, xyzR)
     xs, ys, zs = geodetic2ecef(xyzS[0], xyzS[1], xyzS[2])
-    xi, yi, zi = geodetic2ecef(xyzI[0], xyzI[1], xyzI[2])
-    # Calculate the vertical distance
-    dz = elevR-elevS
+    xr, yr, zr = geodetic2ecef(xyzR[0], xyzR[1], xyzR[2])
 
-    # Calculate the horizontal distance
-    dx = xi - xs
-    dy = yi - ys
-    distance_horizontale = np.sqrt(dx**2 + dy**2)
+    # Calculate the vertical distance
+    dz = xyzR[2] - xyzS[2]
+    dx = xr - xs
+    dy = yr - ys
+
+    dh = np.sqrt(dx**2 + dy**2)
 
     # Calculate the elevation angle
-    beta = np.degrees(np.arctan2(dz, distance_horizontale))
-
+    beta = np.degrees(np.arctan2(dz, dh))
+    beta = np.abs(beta)
     return beta,dz
 
 # Function to find closest ESV value based on dz and beta
@@ -117,22 +88,36 @@ def find_esv_grid(dz_array, angle_array, esv_matrix, beta, dz):
 
 
 def calculate_travel_times_grid(trajectory, grid_points):
-    # Preallocate arrays for speed
     num_grid_points = len(grid_points)
-    travel_times = np.zeros((len(trajectory), num_grid_points))
+    num_trajectory_points = len(trajectory)
+
+    # Convert trajectory and grid_points to numpy arrays for vectorized operations
+    trajectory = np.array(trajectory)
+    grid_points = np.array(grid_points)
+
+    # Preallocate array for speed
+    travel_times = np.zeros((num_trajectory_points, num_grid_points))
+
+    # Convert trajectory to ECEF coordinates
+    xs, ys, zs = geodetic2ecef(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2])
 
     for j, xyzR in enumerate(grid_points):
-        xr, yr, zr = xyzR[0], xyzR[1], xyzR[2]
-        for i, point in enumerate(trajectory):
-            beta, dz = xyz2dec(point, xyzR)
-            print(beta,dz)
-            esv = find_esv_grid(dz_array, angle_array, esv_matrix, beta, dz)
-            xs, ys, zs = geodetic2ecef(point[0], point[1], point[2])
-            travel_time = np.sqrt((xs-xr)**2 + (ys-yr)**2 + (zs-zr)**2) / esv
-            print(f"Shape of travel_time: {np.shape(travel_time)}, Type: {type(travel_time)}")
-            travel_times[i, j] = travel_time
+        xr, yr, zr = xyzR
+
+        # Calculate beta and dz for each trajectory point with respect to the current grid point
+        beta, dz = xyz2dec(trajectory, xyzR)  # Update this function to handle arrays
+
+        # Find ESV values for the given beta and dz
+        esv = find_esv_grid(dz_array, angle_array, esv_matrix, beta, dz)  # Update this function to handle arrays
+
+        # Calculate the squared distance from each trajectory point to the current grid point
+        distances_squared = (xs - xr)**2 + (ys - yr)**2 + (zs - zr)**2
+
+        # Calculate travel time using ESV
+        travel_times[:, j] = np.sqrt(distances_squared) / esv
 
     return travel_times
+
 
 def GNSS_trajectory(lat, lon, elev):
     trajectory = list(zip(lat, lon, elev))
@@ -159,7 +144,6 @@ def calculate_differences(grid_points, lat, lon, elev, time_GNSS, time_DOG, acou
     for j, grid_point in enumerate(grid_points):
         beta, dz = xyz2dec(traj_reel[0], grid_point)
         beta = np.array(beta)
-        beta[beta < 0] = -beta[beta < 0]
 
         esv = find_esv_grid(dz_array, angle_array, esv_matrix, beta, dz)
 
@@ -171,8 +155,6 @@ def calculate_differences(grid_points, lat, lon, elev, time_GNSS, time_DOG, acou
         # plt.show()
 
         difference_data = travel_time - interpolated_acoustic_DOG
-
-
         differences[j] = np.sqrt(np.mean(difference_data ** 2))
 
     return differences
@@ -180,7 +162,11 @@ def calculate_differences(grid_points, lat, lon, elev, time_GNSS, time_DOG, acou
 
 if __name__ == '__main__':
     # Vos données et fonctions préliminaires
-    xyzR = [31.46356378,  291.29858793, 5186.53046237]
+    xyzR =  [  31.46356396,  291.2985875,  5190.77000034] #GNSS1
+    # xyzR = [31.46355667,291.29858588, 5189.86379623] #GNSS2
+    # xyzR = [31.4635628, 291.29857683, 5189.23243428] #GNSS3
+    # xyzR = [31.46357218, 291.29857935, 5190.46271844] #GNSS4
+
 
     data_unit = sio.loadmat('../../data/SwiftNav_Data/Unit1-camp_bis.mat')
     days = data_unit['days'].flatten() - 59015
@@ -208,9 +194,10 @@ if __name__ == '__main__':
     grid_geodetic = [ecef2geodetic(x, y, z) for x, y, z in grid_ecef]
     grid_geodetic = np.array(grid_geodetic)  # Convertir en tableau NumPy pour faciliter l'indexation
     print(grid_geodetic)
-
+    t=time.time()
     differences=(calculate_differences(grid_geodetic, lat, lon, elev, time_GNSS, time_DOG, acoustic_DOG))
     print(differences)
+    print(time.time()-t)
     lat_grid = grid_geodetic[:, 0]
     lon_grid = grid_geodetic[:, 1]
     elev_grid = grid_geodetic[:, 2]
@@ -219,36 +206,37 @@ if __name__ == '__main__':
     y_grid = grid_ecef[:, 1]
     z_grid = grid_ecef[:, 2]
 
-    xR, yR, zR = geodetic2ecef(31.46356378,  291.29858793, 5186.53046237)
+    xR, yR, zR = geodetic2ecef(xyzR[0], xyzR[1], xyzR[2])
 
     plt.figure(figsize=(15, 5))
 
     plt.subplot(1, 3, 1)
-    sc1 = plt.scatter(x_grid, y_grid, c=differences, cmap='Reds', s=100)  # s=50 pour augmenter la taille
+    sc1 = plt.scatter(x_grid, y_grid, c=differences, cmap='jet', s=100)  # s=50 pour augmenter la taille
     plt.scatter(xR, yR, label='Optimal point', s=20, alpha=0.7, edgecolors='black')  # s=20 pour réduire la taille, alpha pour transparence
-    plt.colorbar(sc1).set_label('RMS')
+    plt.colorbar(sc1).set_label('RMS (s)')
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('RMS in X vs Y')
 
     # Scatter plot pour x vs z
     plt.subplot(1, 3, 2)
-    sc2 = plt.scatter(x_grid, z_grid, c=differences, cmap='Reds', s=100)  # s=50 pour augmenter la taille
+    sc2 = plt.scatter(x_grid, z_grid, c=differences, cmap='jet', s=100)  # s=50 pour augmenter la taille
     plt.scatter(xR, zR, label='Optimal point', s=20, alpha=0.7, edgecolors='black')  # s=20 pour réduire la taille, alpha pour transparence
-    plt.colorbar(sc2).set_label('RMS')
+    plt.colorbar(sc2).set_label('RMS (s)')
     plt.xlabel('X')
     plt.ylabel('Z')
     plt.title('RMS in X vs Z')
 
     # Scatter plot pour y vs z
     plt.subplot(1, 3, 3)
-    sc3 = plt.scatter(y_grid, z_grid, c=differences, cmap='Reds', s=100)  # s=50 pour augmenter la taille
+    sc3 = plt.scatter(y_grid, z_grid, c=differences, cmap='jet', s=100)  # s=50 pour augmenter la taille
     plt.scatter(yR, zR, label='Optimal point', s=20, alpha=0.7, edgecolors='black')  # s=20 pour réduire la taille, alpha pour transparence
-    plt.colorbar(sc3).set_label('RMS')
+    plt.colorbar(sc3).set_label('RMS (s)')
     plt.xlabel('Y')
     plt.ylabel('Z')
     plt.title('RMS in Y vs Z')
 
     plt.tight_layout()
     plt.legend()
+    plt.savefig('RMS_1cm_1mm.png',dpi=500)
     plt.show()
