aad35bb8a6730bbe8f174784ad2d5e0398b43a89
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-09-12 21:47:44 -0400

    Building ESV table

--- src/esv/build_esv.py
+++ src/esv/build_esv.py
@@ -7,6 +7,7 @@ from scipy.interpolate import interp1d
 from tqdm import tqdm
 from analytic_sol import *
 import multiprocessing as mp
+import time
 
 def dist_analytic(dz, beta):
     '''
@@ -32,7 +33,7 @@ def dist_analytic(dz, beta):
     t_receiver = None
     best_alpha = None
 
-    while alpha_step > 10e-10:
+    while alpha_step > 10e-8:
         alpha = np.array(alpha_range)
 
         errors_all = np.empty((len(alpha),))
@@ -43,6 +44,13 @@ def dist_analytic(dz, beta):
         for i, angle in enumerate(alpha):
             # Calculate ray path for the given angle
             x, z, t = ray_analytic(cz, depth, gradc, angle)
+            # # Créer un nouveau réseau z pour l'interpolation
+            # z = np.linspace(0, np.max(z_1), int(1e6))
+            #
+            # # Utiliser np.interp pour interpoler les données
+            # x = np.interp(z, z_1, x)
+            # t = np.interp(z, z_1, t)
+
 
             # Calculate errors and find minimum error index
             errors = np.sqrt((xr - x) ** 2 + (zr - z) ** 2)
@@ -59,8 +67,10 @@ def dist_analytic(dz, beta):
             if dist < dist_min:
                 dist_min = dist
                 x_min, z_min = x_all[i], z_all[i]
+                print('zmin',z_min)
                 best_alpha = angle
                 t_receiver = t_all[i]
+            print(dist_min)
 
         # Update previous minimum distance and adjust alpha range for the next iteration
         alpha_range = np.linspace(best_alpha - alpha_step, best_alpha + alpha_step, 100)
@@ -81,7 +91,7 @@ def process_beta(beta, dz):
     return dist_analytic(dz, beta)
 
 def table_angle(dz, step):
-    list_beta = np.arange(45,90,step)
+    list_beta = np.arange(10,90,step)
     print(list_beta)
     c_esv = np.zeros_like(list_beta)
 
@@ -94,13 +104,13 @@ def table_angle(dz, step):
     data = {'list_beta': list_beta.astype(float),
     'c_esv': c_esv.astype(float)}
 
-    filename = f'esv_table_without_tol/data_{dz}_step_angle_{step}.mat'
+    filename = f'castbermuda/data_{dz}_step_angle_{step}.mat'
     savemat(filename, data)
 
 def plot_esv():
 
     # Numéros de fichiers de 5226 à 5249
-    file_numbers = range(5182, 5197)
+    file_numbers = range(5208, 5230)
 
     # Initialisation des listes pour les données des fichiers
     all_angles = []
@@ -139,7 +149,7 @@ def plot_esv():
 
 def plot_and_save_esv():
     # Numéros de fichiers de 5226 à 5249
-    file_numbers = range(5250,5251)
+    file_numbers = range(5200,5251)
 
     # Liste de labels et de couleurs
     labels = []
@@ -148,7 +158,7 @@ def plot_and_save_esv():
 
     for i, file_number in enumerate (file_numbers):
         # Nom du fichier
-        file_name = f"esv_table_without_tol/data_{file_number}_step_angle_2.mat"
+        file_name = f"castbermuda/data_{file_number}_step_angle_5.mat"
 
         # Ajouter le label et la couleur du fichier
         labels.append(f"File {file_number}")
@@ -159,15 +169,15 @@ def plot_and_save_esv():
         angle = data['list_beta']
 
         # Interpolation de 45 à 90 degrés avec un step de 0.01 degrés
-        new_angles = np.arange(45, 90, 0.01)
+        new_angles = np.arange(10, 90, 0.01)
         f = interp1d(angle.flatten(), esv.flatten(), kind='cubic',bounds_error=False, fill_value='extrapolate')
         new_esv = f(new_angles)
 
         # Enregistrer les données interpolées dans un fichier avec le nom reflétant le step de 0.01
-        new_file_name = f"esv_table_without_tol/mat_interp/data_{file_number}_step_angle_0_01.mat"
+        new_file_name = f"castbermuda/mat_interp/data_{file_number}_step_angle_0_01.mat"
         sio.savemat(new_file_name, {'elevation angle (deg)': new_angles, 'esv (m/s)': new_esv})
 
-        plt.plot(new_angles, new_esv, label=labels[i], color=colors[i])
+        plt.scatter(new_angles, new_esv, label=labels[i], color=colors[i])
 
     plt.xlabel('Angle (degrees)')
     plt.ylabel('ESV')
@@ -184,7 +194,7 @@ def compare_interpolations(file_number=5225):
     angle = data['list_beta']
 
     # Interpolation de 45 à 90 degrés avec un step de 0.01 degrés
-    new_angles = np.arange(45, 90, 0.01)
+    new_angles = np.arange(10, 90, 0.01)
 
     # Liste des méthodes d'interpolation possibles
     # interpolation_methods = ['linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic']
@@ -208,11 +218,17 @@ def compare_interpolations(file_number=5225):
 
 
 if __name__ == '__main__':
-    # table_angle(5130,2)
-    # table_angle(5250,2)
-    # for k in range (8):
-    #     table_angle(5250+k,2)
+    t = time.time()
+    table_angle(5250,5)
+    # print(time.time()-t)
+
+    # table_angle(5248,5)
+    # table_angle(5249,5)
+    # table_angle(5250,5)
+    # for k in range (50):
+    #     table_angle(5200+k,5)
+
     # plot_esv()
     plot_and_save_esv()
 
-    compare_interpolations(file_number=5225)
+    # compare_interpolations(file_number=5225)
