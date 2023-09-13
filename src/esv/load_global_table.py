a975d22344f5e73d6fc73a8ea173da0047843013
ThaliaGueroult <thalia.gueroult@ensta-bretagne.org>
2023-09-12 22:47:45 -0400

    Load esv table and plot it

--- src/esv/load_global_table.py
+++ src/esv/load_global_table.py
@@ -2,9 +2,11 @@
 
 from scipy.io import loadmat
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Charger les données depuis le fichier .mat
-dossier = 'esv_table_without_tol/global_table_interp'
+dossier = 'GDEM/global_table_interp'
 output_file_path = os.path.join(dossier, 'global_table_esv.mat')
 loaded_data = loadmat(output_file_path)
 
@@ -19,31 +21,44 @@ print("\nMatrice:")
 print(loaded_data['matrice'])
 
 
-import numpy as np
-import scipy.io as sio
+import matplotlib.pyplot as plt
+import os
+from scipy.io import loadmat
 
-# Charger les données depuis le fichier .prd
-data_prd = np.genfromtxt('../../data/SwiftNav_Data/Unit4-camp.prd')
-days = data_prd[:,0].flatten()
-times = data_prd[:,1].flatten()
-lat = data_prd[:,5].flatten()
-lon = data_prd[:,6].flatten()
-elev = data_prd[:,7].flatten()
-x = data_prd[:,2].flatten()
-y = data_prd[:,3].flatten()
-z = data_prd[:,4].flatten()
+# Charger les données depuis le fichier .mat
+dossier = 'GDEM/global_table_interp'
+output_file_path = os.path.join(dossier, 'global_table_esv.mat')
+loaded_data = loadmat(output_file_path)
 
-# Enregistrer ces données dans un fichier .mat
-mat_file_path = '../../data/SwiftNav_Data/Unit4-camp_bis.mat'
-sio.savemat(mat_file_path, {
-    'days': days,
-    'times': times,
-    'lat': lat,
-    'lon': lon,
-    'elev': elev,
-    'x': x,
-    'y': y,
-    'z': z
-})
+# Extraire les données
+angle = loaded_data['angle'].squeeze()
+distance = loaded_data['distance'].squeeze()
+matrice = loaded_data['matrice']
 
-print(f"Les données ont été sauvegardées dans {mat_file_path}")
+# Créer un colormap pour les différentes distances
+colors = plt.cm.rainbow(np.linspace(0, 1, len(distance)))
+
+plt.figure(figsize=(12, 8))
+
+# Pour chaque distance, plotter la ligne correspondante de la matrice
+for i, dist in enumerate(distance):
+    plt.plot(angle, matrice[i], color=colors[i])
+
+# Convertir les distances à une échelle normalisée
+normalized_distances = (distance - distance.min()) / (distance.max() - distance.min())
+
+tick_interval = 5
+selected_indices = np.arange(0, len(distance), tick_interval)
+selected_ticks = (selected_indices / len(distance)).tolist()
+selected_labels = [distance[idx] for idx in selected_indices] # nous excluons le dernier élément pour éviter un dépassement d'indice
+
+cbar = plt.colorbar(plt.cm.ScalarMappable(cmap='rainbow'), ticks=selected_ticks)
+cbar.set_label('Vertical Distance (m)')
+cbar.set_ticklabels(selected_labels)
+
+plt.xlabel('Angle (°)')
+plt.ylabel('Effective sound velocity (m/s)')
+plt.title('Effective Sound velocity table look-up for Bermuda cast')
+
+plt.tight_layout()
+plt.show()
